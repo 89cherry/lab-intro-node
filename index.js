c5a5b0c6de4983070b25e155092136f85b72120b
Diego Cerezo <diego.cerezo@gmail.com>
2024-05-05 17:21:51 +0200

    solved

--- index.js
+++ index.js
@@ -1,17 +1,34 @@
 class SortedList {
-  constructor() {}
+  constructor(items, length) {
+    this.items = []
+    this.length = this.items.length
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item)
+    this.items.sort((a, b) => a - b)
+    this.length = this.items.length
 
-  get(pos) {}
+  }
 
-  max() {}
+  get(pos) {
+    if (pos === undefined) {
+      throw new Error("OutOfBounds")
+    }
+    return this.items[pos]
+  }
 
-  min() {}
+  max() {
 
-  sum() {}
+    Math.max(this.items)
+  }
 
-  avg() {}
+
+  min() { }
+
+  sum() { }
+
+  avg() { }
 }
 
 module.exports = SortedList;
